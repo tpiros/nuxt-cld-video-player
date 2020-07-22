f2fb586fc6776d3cbb6d73c16ce36ed5d7895a45
tpiros <2042718+tpiros@users.noreply.github.com>
2020-07-22 21:42:30 +0200

    first commit

---
+++ components/videoPlayer.vue
@@ -0,0 +1,25 @@
+<template>
+  <video
+    id="example-player"
+    controls
+    width="400"
+    muted
+    class="cld-video-player cld-video-player-skin-dark"
+  ></video>
+</template>
+<script>
+export default {
+  mounted() {
+    if (process.client) {
+      const cld = cloudinary.Cloudinary.new({
+        cloud_name: "tamas-demo",
+        secure: true
+      });
+      const demoplayer = cld.videoPlayer("example-player");
+      demoplayer.source("web-perf/IMG_0460", {
+        transformation: { width: 400 }
+      });
+    }
+  }
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <client-only>
+    <div>
+      <video-player />
+    </div>
+  </client-only>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      script: [
+        {
+          vmid: 'extscript',
+          src:
+            "https://unpkg.com/cloudinary-core@2.10.3/cloudinary-core-shrinkwrap.min.js",
+          defer: true,
+        },
+        {
+          vmid: 'extscript',
+          src:
+            "https://unpkg.com/cloudinary-video-player@1.4.1/dist/cld-video-player.min.js",
+          defer: true,
+        }
+      ]
+    };
+  }
+};
+</script>
